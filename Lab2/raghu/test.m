cc2308426b231dda13f6bb26c877a2b540840404
raghu129 <kvraghu.129@gmail.com>
2018-02-19 19:19:27 +0100

    no message

--- Lab2/raghu/test.m
+++ Lab2/raghu/test.m
@@ -1,7 +1,7 @@
 %% RBF Batchlearning
 clc; close all; clear;
 
-units = 63;
+units = 70;
 
 xTrain = (0:0.1:2*pi)';
 xTest = (0.05:0.1:2*pi)';
@@ -13,27 +13,26 @@ train_size=length(sinTrainTarget);
 test_size = length(sinTestTarget);
 
 %plot( xTrain, squareTrainTarget);
-W = rand(units,1);
+%W = rand(units,1);
 mu = 0.5;
-var = 0.5;
+var = 0.01;
 sDev = sqrt(var);
-%eta = 0.001;
+
 max_error = 0.1;
 
-epoch = 10000;
-
 xTrainMat = repmat(xTrain,[1,units]);
-muMat = repmat(rand(1,units), [train_size, 1]);
+mean = (0:(2*pi/(units-1)):2*pi);
+muMat = repmat(mean, [train_size, 1]);
 sDevMat = repmat(sqrt(rand(1,units)), [train_size, 1]);
 
-Phi = exp((-(xTrainMat-mu).^2)./(2*sDev.^2));
-%Phi = exp((-(xTrainMat-muMat).^2)/(2*sDev^2));
-%output = sum(Phi'*W, 2);
+Phi = exp((-(xTrainMat-muMat).^2)./(2*sDev.^2));
 
-f_calc = Phi*W;
 newW = (inv(Phi'*Phi))*Phi'*sinTrainTarget;
-sum(Phi*newW-sinTrainTarget)
-plot(Phi*newW)
-hold on
-plot(sinTrainTarget)
-hold off
+sum((Phi*newW-sinTrainTarget).^2)/length(sinTrainTarget)
+    
+%plot(Phi*newW)
+%hold on
+%plot(sinTrainTarget)
+%hold off
+
+
