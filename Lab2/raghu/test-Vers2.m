ea685c037f58df26312eb362cf425108f7b4602e
XGuan <xguan@kth.se>
2018-02-22 16:09:00 +0100

    minor change

--- Lab2/raghu/test-Vers2.m
+++ Lab2/raghu/test-Vers2.m
@@ -2,7 +2,7 @@
 clc; close all; clear;
 
 units = 29;
-
+rng(4)
 
 xTrain = (0:0.1:2*pi)';
 noise = normrnd(0, sqrt(0.1), [length(xTrain), 1]);
@@ -15,7 +15,7 @@ noise1 = normrnd(0, sqrt(0.1), [length(xTest), 1]);
 
 sinTrainTarget = sin(2*(xTrain)) + noise;
 % squareTrainTarget = square(2*xTrain);
-sinTestTarget = sin(2*(xTest));
+sinTestTarget = sin(2*(xTest ));
 % squareTestTarget = square(2*xTest);
 train_size=length(sinTrainTarget);
 test_size = length(sinTestTarget);
@@ -23,19 +23,16 @@ test_size = length(sinTestTarget);
 %plot( xTrain, squareTrainTarget);
 %W = rand(units,1);0,1
 %mu = 0.5;
-var = 0.5;
+var = 0.10;
 sDev = sqrt(var);
-
-max_error = 0.1;
 
 xTrainMat = repmat(xTrain,[1,units]);
 range = 2*pi;
 mean = (0:(range/(units-1)):range);
-%mean = sort(2*pi*rand([1 units]))
+mean = sort(2*pi*rand([1 units]));
 %mean = mean/0.9
 muMat = repmat(mean, [train_size, 1]);
 % sDevMat = repmat(sqrt(rand(1,units)), [train_size, 1]);
-
 
 Phi = exp((-(xTrainMat-muMat).^2)./(2*sDev.^2));
 
@@ -50,8 +47,9 @@ hold on
 %yyaxis left           % plot against left y-axis 
 %plot(xTrain, sinTrainTarget)        
 %plot(xTrain, Phi*newW)
-plot(xTest, newPhi*newW)
 plot(xTest, sinTestTarget)
+plot(xTest, newPhi*newW)
+
 legend('TestData','Testfit')
 hold off
 %plot(Phi*newW)
@@ -59,3 +57,22 @@ hold off
 %plot(sinTrainTarget)
 %hold off
 
+
+%{
+W = zeros(1,units);
+for n = 1:epoch
+    output = zeros(1,units);
+    Phi = exp(-(xTrain-mu).^2/(2*sDev^2));
+    output = sum(Phi'*W, 2);
+    dW = -eta*(output'-sinTrainTarget)*Phi';
+    W = W + dW;
+    V = output' - sinTrainTarget;
+    error = sum(V)/length(V);
+    if (error < 0.1)
+        disp('Number of iterations:');
+        disp(n);
+        break;
+    end
+    oldW = W;
+end
+%}
