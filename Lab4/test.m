ae0e4e97d180a4ae162d8c6f7e8811983eac001a
raghu129 <kvraghu.129@gmail.com>
2018-03-21 08:41:02 +0100

    test

--- Lab4/test.m
+++ Lab4/test.m
@@ -6,12 +6,15 @@ testData = bindata_trn;
 
 N = 8000;
 V = 784;
-H = 50;
-rbm = randRBM(V,H);
+H = 150;
+nodes = [V H];
+rbm = randDBN(nodes);
 
 opts.MaxIter = 100;
 opts.BatchSize = 100;
 opts.Verbose = true;
 opts.StepRatio = 0.1;
-rbm = pretrainRBM(rbm, traindata, opts);
+rbm = pretrainDBN(rbm, traindata, opts);
+
+plot();
 
